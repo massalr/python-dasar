f55412fbb8e58cd558da81b4ca4760f76ac975fb
massalr <lookingforyourname@gmail.com>
2022-05-09 15:52:18 +0700

    firs commit

---
+++ 01-variable.py
@@ -0,0 +1,10 @@
+a = 10 # memasukan angka sepuluh ke variable a jadi 10 = a, 10 adalah value
+b = a + 5
+print(f"nilai 10 + 5 adalah : {b}")
+# maksud di atas adalah variable b menyimpan hasil 10 + 5
+# = disebut assigment
+
+# spesifik menggunakan integer
+a = int(20)
+print(a)
+print(type(a))
