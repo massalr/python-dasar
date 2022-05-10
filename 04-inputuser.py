b0da34ed5ac350094f8ddf7ac65565c21538e655
massalr <lookingforyourname@gmail.com>
2022-05-10 18:53:37 +0700

    input user

---
+++ 04-inputuser.py
@@ -0,0 +1,12 @@
+# data = input("masukan data: ")
+# print("data adalah :", data," type data :", type(data))
+
+# # mengambil data tipe integer
+# angka = int(input("masukan angka: "))
+# print("angka adalah:", angka, "type data:", type(angka))
+
+
+# untuk data boolean di casting dulu ke int lalu ke bool
+biner = bool(int(input("masukan nilai bool :")))
+
+print("data = ", biner, "type ", type(biner))
