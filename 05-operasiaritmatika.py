3f51caab2eebf035cbdfd64ea4adf4dcc379f7f2
massalr <lookingforyourname@gmail.com>
2022-05-10 19:03:01 +0700

    operasi aritmatika

---
+++ 05-operasiaritmatika.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# operasi tambah +
+hasil = a + b
+print(f"{a} + {b} = {hasil}")
+
+# operasi kurang -
+hasil = a - b
+print(f"{a} - {b} = {hasil}")
+
+# operasi perkalian *
+hasil = a * b
+print(f"{a} * {b} = {hasil}")
+
+# operasi pembagian /
+hasil = a / b
+print(f"{a} / {b} = {hasil}")
+
+# operasi eksponen (pangkat) **
+hasil = a ** b
+print(f"{a} ** {b} = {hasil}")
+
+# operasi modulus %
+hasil = a % b
+print(f"{a} % {b} = {hasil}")
+
+# operasi floor division //
+hasil = a // b
+print(f"{a} // {b} = {hasil}")
