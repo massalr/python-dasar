51b96339ff986275d09d0f1b573e5ed371d5d1e8
massalr <lookingforyourname@gmail.com>
2022-05-10 18:43:25 +0700

    cating tipe data

---
+++ 03-castingtipedata.py
@@ -0,0 +1,35 @@
+# casting tipe data merubah satu tipe ke tipe yang lain
+# tipe data = int,float,str,bool
+
+print("======================= integer")
+data_int = 9
+print(f"data = {data_int}", type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print(f"data = {data_float}", type(data_float))
+print(f"data = {data_str}", type(data_str))
+print(f"data = {data_bool}", type(data_bool))
+
+print("======================== float")
+data_float = 9.9
+print(f"data = {data_float}", type(data_float))
+
+data_float = float(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print(f"data = {data_int}", type(data_int))
+print(f"data = {data_str}", type(data_str))
+print(f"data = {data_bool}", type(data_bool))
+
+print("======================== string")
+data_str = "namakamu"
+print(f"data = {data_str}", type(data_str))
+
+# data_float = float(data_str)
+data_int = str(data_int)
+data_bool = bool(data_str)
+print(f"data = {data_int}", type(data_int))
+print(f"data = {data_str}", type(data_str))
+print(f"data = {data_bool}", type(data_bool))
