5b7bbf5334a15757d4ca2f2988a4ab98950b6e6c
massalr <lookingforyourname@gmail.com>
2022-05-11 20:56:18 +0700

    operasi dan manipulasi string

---
+++ 11-operasidanmanipulasistring.py
@@ -0,0 +1,52 @@
+# operasi dan manipulasi string
+
+# menyambung string (concatenate)
+nama_pertama = "computer"
+nama_tengah = "D"
+nama_akhir = "error"
+
+nama_lengkap = nama_pertama + " " + nama_tengah + "'" + nama_akhir
+print(nama_lengkap)
+
+# menghitung panjang string
+panjang = len(nama_lengkap)
+print("panjang dari " + nama_lengkap + "=" + str(panjang))
+
+# operator untuk string
+# mengecek apakah ada komponen char di string
+r = "r"
+status = r in nama_lengkap
+print(r + " ada di " + nama_lengkap + "=" + str(status))
+
+# mengulang string
+print("ty"*10)
+
+# indexing
+print("index ke-0 : " + nama_lengkap[0])
+print("index ke-(-3) : " + nama_lengkap[-3])
+print("index ke-[3:6] : " + nama_lengkap[3:7])
+
+# item paling kecil
+print("paling kecil : " + min(nama_lengkap))
+
+# item paling besar
+print("paling besar : " + max(nama_lengkap))
+
+assci_code = ord(" ")
+print("ASCII code untuk spasi : " + str(assci_code))
+data = 119
+print("char untuk ASCII code 119 : " + chr(data))
+
+# operator dalam bentuk method
+nama = "computer error"
+nama = nama.count(r)
+print(nama)
+nama = "computer error"
+nama = nama.upper()
+print(nama)
+
+# pengecekan isX method
+
+salam = "hallo"
+apakah_lower = salam.islower()
+print(salam + " apakah lower : " + str(apakah_lower))
