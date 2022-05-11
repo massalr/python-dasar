899c776f3791a7eb14241624de66880fb0fc9673
massalr <lookingforyourname@gmail.com>
2022-05-11 17:00:34 +0700

    operasi assigment

---
+++ 09-operatorasignment.py
@@ -0,0 +1,29 @@
+# operasi yang dapat dilakukan dengan penyingkatan
+# operasi di tambah dengan assigment
+
+a = 5 # adalah assigment
+print("nilai a = ", a)
+
+a += 1 # artinya adalah a = a + 1
+print("nilai a += 1, nilai a menjadi", a)
+
+a -= 2 # artinya adalah a = a - 2
+print("nilai a -= 2, nilai a menjadi", a)
+
+a *= 5 # artinya adalah a = a * 5
+print("nilai a *=, nilai a menjadi", a)
+
+a /= 2 # artinya adalah a = a / 2
+print("nilai a /=, nilainya adalah", a)
+
+b = 10
+print("\nnilai b =", b)
+
+b %= 3
+print("nilai b%= 3, nilai b menjadi", b)
+
+b = 10
+print("\nnilai b =", b)
+
+b //= 3
+print("nilai b //= 3, nilai b menjadi", b)
