0b987a5f4732d3ee5e1a8f5dd61aa82269d6d91b
massalr <lookingforyourname@gmail.com>
2022-05-11 17:22:26 +0700

    pengenalan string

---
+++ 10-pengenalanstring.py
@@ -0,0 +1,46 @@
+# cara membuat string
+# srting adalah kumpulan karakter
+
+data = "ini adalah string"
+print(data, type(data))
+
+# menggunakan single quote '...'
+data = 'menggunakan single quote'
+print(data)
+
+# menggunakan double quote "..."
+data = "menggunakan double quote"
+print(data)
+
+print("ini adalah hari jum'at")
+
+# menggunakan tanda \
+print('hari ini hari jum\'at')
+
+# backslash
+print("C:\\user\\Computer")
+
+# tab
+print("computer\terror")
+
+# backspace
+print("computer \berror")
+
+# newline
+print("baris pertama.\nbaris kedua.") # LF = line feed
+
+# string literal atau raw
+print(r"C:\new\folder")
+
+# multiline literal string
+print("""
+nama : nama
+kelas : kelas
+""")
+
+# multiline literal string dan RAW
+print(r"""
+nama : nama
+kelas : kelas
+website : www.computererror.com/nama
+""")
