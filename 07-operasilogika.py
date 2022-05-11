5ab45c52379ece5bc349ff616beca93f92874c4d
massalr <lookingforyourname@gmail.com>
2022-05-11 13:17:46 +0700

    operator logika boolean

---
+++ 07-operasilogika.py
@@ -0,0 +1,23 @@
+# operasi logika atau boolean
+# not, or, and, xor
+
+print("=======not")
+a = True
+c = not a
+print(f"data a = {a}")
+print("--------------")
+print(f"data c = {c}")
+
+# jika salah satu True maka hasilnya True
+print("=======or")
+a = True
+b = False
+c = a or b
+print(f"{a} or {b} = {c}")
+
+# jika 2 True maka hasilnya True
+print("=======and")
+a = True
+b = False
+c = a and b
+print(f"{a} and {b} = {c}")
