7a1d616a1f02ffd9c3479449a35e8b9aa7357193
massalr <lookingforyourname@gmail.com>
2022-07-17 11:29:52 +0700

    perulangan for

---
+++ 16-perulanganfor.py
@@ -0,0 +1,24 @@
+# perulangan
+
+# perulangan menggunakan list
+angka = [0,1,2,3,4,5]
+
+for i in angka:
+    print(f"angka perulangan --> {i}")
+
+print("selesai \n")
+
+# perulangan menggunakan range
+angka_range = range(5)
+
+for i in angka_range:
+    print(f"angka perulangan --> {i}")
+
+print("selesai \n")
+
+# perulangan string
+
+data_str = "teh manis hangat"
+
+for huruf in data_str:
+    print(huruf)
