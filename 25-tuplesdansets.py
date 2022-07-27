8309a2217fccd58b6ecf1930a2b25ad91c20fcdb
massalr <lookingforyourname@gmail.com>
2022-07-27 09:00:13 +0700

    tuples dan sets

---
+++ 25-tuplesdansets.py
@@ -0,0 +1,11 @@
+# list
+data_list = [1,2,3,4,4,5]
+print(f"ini data list {data_list}")
+
+# tuples bersifat imutable atau tidak bisa dirubah item nya
+data_tuples = (3,4,5,6,7)
+print(f"ini data tuples {data_tuples}")
+
+# sets adalah himpunan data tapi tidak punya index
+data_sets = {3,4,5,7}
+print(f"ini data sets {data_sets}")
